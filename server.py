4d6c083ae1d9b9f75e59c2039295ee26f5b2bc97
Purpi123 <108231225+Purpi123@users.noreply.github.com>
2025-06-08 20:48:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import time
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -45,4 +46,5 @@ def get_command():
     return ''
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
