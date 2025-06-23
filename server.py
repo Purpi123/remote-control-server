3b1fcff0a80d5d60d60fa9d74946439b293d20a9
Purpi123 <108231225+Purpi123@users.noreply.github.com>
2025-06-24 00:37:06 +0200

    Update server.py

--- server.py
+++ server.py
@@ -76,9 +76,10 @@ def send_command():
     hide = data.get("hide", None) # Get taskbar hide status
     effect = data.get("effect", None) # For GDI effects
     state_value = data.get("state", None) # For GDI effects on/off
+    effects_list = data.get("effects", None) # For set_gdi_effects
 
     if client_id and cmd:
-        client_commands[client_id] = {"cmd": cmd, "title": title, "message": message, "icon": icon, "buttons": buttons, "topmost": topmost, "monitor_index": monitor_index, "image": image, "pid": pid, "image_type": image_type, "filters": filters, "actions": actions, "hide": hide, "url": data.get("url", None), "process": data.get("process", None), "effect": effect, "state": state_value}
+        client_commands[client_id] = {"cmd": cmd, "title": title, "message": message, "icon": icon, "buttons": buttons, "topmost": topmost, "monitor_index": monitor_index, "image": image, "pid": pid, "image_type": image_type, "filters": filters, "actions": actions, "hide": hide, "url": data.get("url", None), "process": data.get("process", None), "effect": effect, "state": state_value, "effects": effects_list}
         if cmd == "open_url":
             print(f"[SERVER] open_url command for {client_id}: {data.get('url')}")
         elif cmd == "open_process":
